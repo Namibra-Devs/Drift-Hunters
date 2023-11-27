7437bb9de4f0ee1b449b3a82f5ac6d0795ac5ce3
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2023-11-27 14:08:29 +0000

    html structured

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,12 +1,386 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Black Bike</title>
 </head>
+
 <body>
-    
+    {{-- header starts --}}
+    <header>
+        <div>
+            {{-- right logo wrapper --}}
+            <div>
+                <div>
+                    <a href=""><img src="{{ asset('assets/frontend/images/logo.jpg') }}" alt=""></a>
+                </div>
+            </div>
+
+            <div>
+                <ul>
+                    <li><a href="">login</a></li>
+                    <li><a href="">Schedule</a></li>
+                    <li><a href="">Merchs</a></li>
+                    <li><a href="">Get your Tickets</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    {{-- haeader ends --}}
+
+    {{-- hero section starts --}}
+    <section>
+        <div>
+            <div>
+                <div>
+                    <h1> Unity of Two Wheels</h1>
+                    <a href="">Get Your Tickets</a>
+                </div>
+
+                <div>
+                    <span class="time">9:00AM - 11:00AM</span> <br>
+                    <span class="date">24TH NOBEMBER, 2023</span>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- hero section ends --}}
+
+    <section>
+        <div>
+            <h1>TICKETS NOW AVAILABLE</h1>
+        </div>
+        <div>
+            <h1>TICKETS NOW AVAILABLE</h1>
+        </div>
+    </section>
+
+    {{-- feacturing section --}}
+    <section>
+        <div class="section-title">
+            <div>
+                <img src="{{ asset('assets/frontend/images/white-play.jpg') }}" alt="">
+                <h2>FEATURING</h2>
+            </div>
+            <div>
+                <img src="{{ asset('assets/frontend/images/rider.jpg') }}" alt="">
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <div>
+                    <div class="feature-image">
+                        <img src="{{ asset('assets/frontend/images/feature-img1.jpg') }}" alt="">
+                    </div>
+                    <div class="content">
+                        <h2 class="feature-title">VENDOR EXPO</h2>
+                        <p>As you wander through the Expo, take in the vibrant colors, the hum of conversations, and the
+                            scent of leather and engine oil in the air. It's not just a shopping excursion; it's a
+                            sensory journey that adds an extra layer of excitement to the overall experience.</p>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="feature-image">
+                        <img src="{{ asset('assets/frontend/images/feature-img2.jpg') }}" alt="">
+                    </div>
+                    <div class="content">
+                        <h2 class="feature-title">VENDOR EXPO</h2>
+                        <p>As you wander through the Expo, take in the vibrant colors, the hum of conversations, and the
+                            scent of leather and engine oil in the air. It's not just a shopping excursion; it's a
+                            sensory journey that adds an extra layer of excitement to the overall experience.</p>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="feature-image">
+                        <img src="{{ asset('assets/frontend/images/feature-img3.jpg') }}" alt="">
+                    </div>
+                    <div class="content">
+                        <h2 class="feature-title">VENDOR EXPO</h2>
+                        <p>As you wander through the Expo, take in the vibrant colors, the hum of conversations, and the
+                            scent of leather and engine oil in the air. It's not just a shopping excursion; it's a
+                            sensory journey that adds an extra layer of excitement to the overall experience.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    {{-- featuring section ends --}}
+
+    {{-- tickets section --}}
+    <section>
+        <div class="section-title">
+            <h1>COUPE YOUR TICKET</h1>
+        </div>
+
+        <div>
+            <div>
+                <div class="ticket">
+                    <div class="ticket-header">
+                        <h2>GOLD</h2>
+                        <h1>$100</h1>
+                    </div>
+                    <div class="description">
+                        <span> <i></i>Entry to all areas</span>
+                        <span> <i></i>Participated in non tickets</span>
+                        <span> <i></i>Acess to live entertainment</span>
+                        <span> <i></i>Activities and access to live entertainment</span>
+                    </div>
+                    <div>
+                        <a href="">BUY NOW</a>
+                    </div>
+                </div>
+                <div class="ticket">
+                    <div class="ticket-header">
+                        <h2>PLATINUM</h2>
+                        <h1>$200</h1>
+                    </div>
+                    <div class="description">
+                        <span> <i></i>Entry to all areas</span>
+                        <span> <i></i>Participated in non tickets</span>
+                        <span> <i></i>Acess to live entertainment</span>
+                        <span> <i></i>Activities and access to live entertainment</span>
+                    </div>
+                    <div>
+                        <a href="">BUY NOW</a>
+                    </div>
+                </div>
+                <div class="ticket">
+                    <div class="ticket-header">
+                        <h2>DIAMOND</h2>
+                        <h1>$500</h1>
+                    </div>
+                    <div class="description">
+                        <span> <i></i>Entry to all areas</span>
+                        <span> <i></i>Participated in non tickets</span>
+                        <span> <i></i>Acess to live entertainment</span>
+                        <span> <i></i>Activities and access to live entertainment</span>
+                    </div>
+                    <div>
+                        <a href="">BUY NOW</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- Tickets section end --}}
+
+    <section>
+        <div>
+            <div>
+                <div>
+                    <h1>LIVE GUEST CELEBRITY LINE UP</h1>
+                    <p>Get ready for an adrenaline-fueled journey! This annual extravaganza isn't just a gathering; it's
+                        a celebration of freedom, unity, and the roaring symphony of bikes echoing against the vibrant
+                        landscapes of Ghana.</p>
+                </div>
+                <div>
+                    <a href="">GET YOUR TICKET</a>
+                </div>
+            </div>
+
+            <div>
+                <div><img src="{{ asset('assets/frontend/images/celeb-img1.png') }}" alt=""></div>
+                <div><img src="{{ asset('assets/frontend/images/celeb-img2.png') }}" alt=""></div>
+                <div><img src="{{ asset('assets/frontend/images/celeb-img3.png') }}" alt=""></div>
+                <div><img src="{{ asset('assets/frontend/images/celeb-img4.png') }}" alt=""></div>
+                <div><img src="{{ asset('assets/frontend/images/celeb-img5.png') }}" alt=""></div>
+                <div><img src="{{ asset('assets/frontend/images/celeb-img6.png') }}" alt=""></div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="section-title">
+            <img src="{{ asset('assets/frontend/images/wine-play.png') }}" alt="">
+            <h1>GET INVOLVED</h1>
+        </div>
+
+        <div>
+            <div>
+                <div>
+                    <img src="{{ asset('assets/frontend/images/bike1.png') }}" alt="">
+                </div>
+                <div>
+                    <h2>BECOME A SPONSOR</h2>
+                    <p> Become a sponsor and align your brand with the spirit of adventure, unity, and freedom that
+                        defines this extraordinary event.</p>
+
+                    <a href="">REGISTER YOUR INTEREST</a>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <img src="{{ asset('assets/frontend/images/bike2.png') }}" alt="">
+                </div>
+                <div>
+                    <h2>EXHIBIT LIVE</h2>
+                    <p>Join the vibrant Vendor Expo at Ghana Black Bike Week and put your products or services in front
+                        of a diverse and engaged audience.</p>
+
+                    <a href="">APPLY NOW</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    {{-- products section --}}
+    <section>
+        <div class="section-title">
+            <div>
+                <h1>BIKE IN <span>STYLE!</span></h1>
+                <h1>ROKE IN <span>STYLE</span></h1>
+            </div>
+
+            <div>
+                <img src="{{ asset('assets/frontend/images/rider.jpg') }}" alt="">
+            </div>
+        </div>
+
+        <div>
+            {{-- products --}}
+            <div>
+                {{-- product --}}
+                <div>
+                    <div>
+                        <img src="{{ asset('assets/frontend/images/product1.png') }}" alt="">
+                    </div>
+                    <div>
+                        <h2 class="product-name">Bike Life Dream Chasers Short Sleeves</h2>
+                        <h3 class="product-price">$50</h3>
+                    </div>
+                    <div>
+                        <a href="">VIEW PRODUCT</a>
+                    </div>
+                </div>
+                {{-- product --}}
+                <div>
+                    <div>
+                        <img src="{{ asset('assets/frontend/images/product1.png') }}" alt="">
+                    </div>
+                    <div>
+                        <h2 class="product-name">Bike Life Detached Armless Jacket 1</h2>
+                        <h3 class="product-price">$50</h3>
+                    </div>
+                    <div>
+                        <a href="">VIEW PRODUCT</a>
+                    </div>
+                </div>
+                {{-- product --}}
+                <div>
+                    <div>
+                        <img src="{{ asset('assets/frontend/images/product1.png') }}" alt="">
+                    </div>
+                    <div>
+                        <h2 class="product-name">Bike Life Detached Armless Jacket 2</h2>
+                        <h3 class="product-price">$50</h3>
+                    </div>
+                    <div>
+                        <a href="">VIEW PRODUCT</a>
+                    </div>
+                </div>
+                {{-- product --}}
+                <div>
+                    <div>
+                        <img src="{{ asset('assets/frontend/images/product1.png') }}" alt="">
+                    </div>
+                    <div>
+                        <h2 class="product-name">Bike Life Dream Chasers Short Sleeves</h2>
+                        <h3 class="product-price">$50</h3>
+                    </div>
+                    <div>
+                        <a href="">VIEW PRODUCT</a>
+                    </div>
+                </div>
+                {{-- product --}}
+                <div>
+                    <div>
+                        <img src="{{ asset('assets/frontend/images/product1.png') }}" alt="">
+                    </div>
+                    <div>
+                        <h2 class="product-name">Bike Life Dream Chasers Short Sleeves</h2>
+                        <h3 class="product-price">$50</h3>
+                    </div>
+                    <div>
+                        <a href="">VIEW PRODUCT</a>
+                    </div>
+                </div>
+                {{-- product --}}
+                <div>
+                    <div>
+                        <img src="{{ asset('assets/frontend/images/product1.png') }}" alt="">
+                    </div>
+                    <div>
+                        <h2 class="product-name">Bike Life Pants</h2>
+                        <h3 class="product-price">$50</h3>
+                    </div>
+                    <div>
+                        <a href="">VIEW PRODUCT</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    {{-- sponsors section --}}
+    <section>
+        <div class="section-title">
+            <h1>OUR SPONSORS</h1>
+        </div>
+
+        {{-- sponsors --}}
+        <div>
+            <div><h2>RACELORDS</h2></div>
+            <div><h2>RITROFITS</h2></div>
+            <div><h2>RACELORDS</h2></div>
+            <div><h2>RITROFITS</h2></div>
+            <div><h2>RACELORDS</h2></div>
+            <div><h2>RITROFITS</h2></div>
+            <div><h2>RACELORDS</h2></div>
+            <div><h2>RITROFITS</h2></div>
+        </div>
+    </section>
+
+    {{-- footer starts --}}
+    <section>
+        <div>
+            <div>
+                <div>
+                    <img src="{{ asset('assets/frontend/images/logo.jpg') }}" alt="">
+                    <pre>Asamoah Gyan Centre,
+                        Kumasi, Ghana.</pre>
+                        <p><span class="event-time">9:00AM - 10:00AM</span> <br>
+                        <span class="event-date">11th November, 2023</span></p>
+                </div>
+
+                <div>
+                    <h2>STAY CONNECTED</h2>
+                    <div>
+                        <i>twitter</i>
+                        <i>linkedin</i>
+                        <i>instagram</i>
+                    </div>
+                </div>
+
+                <div>
+                    <h2>REGISTER FOR THE EVENT</h2>
+                    <a href="">GET YOUR TICKETS</a>
+                </div>
+                <hr>
+                <div>
+                    <p>&copy; Black Bike 2023</p>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
+
 </html>
