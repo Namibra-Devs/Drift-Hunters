5893998b52c23d3841456cdce3543f2d5740cc2b
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2023-11-30 13:27:21 +0000

    admin routes and middlewares set

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Permalink;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,13 +15,49 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('frontend.index');
+  return view('frontend.index');
 });
 
 /*=======================================================
 ******************** Admin Routes **********************
 =======================================================*/
 
-Route::controller(LoginController::class)->group(function () {
-    Route::get('/admin', 'login')->name('admin.auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'guest:admin'], function () {
+  Route::post('/login', 'Admin\LoginController@authenticate')->name('admin.auth');
+
+  Route::get('/mail-form', 'Admin\ForgetController@mailForm')->name('admin.forget.form');
+  Route::post('/sendmail', 'Admin\ForgetController@sendmail')->name('admin.forget.mail');
 });
+
+
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth:admin', 'checkstatus', 'setLfmPath']], function () {
+
+  // Admin logout Route
+  Route::get('/logout', 'Admin\LoginController@logout')->name('admin.logout');
+
+  Route::group(['middleware' => 'checkpermission:Dashboard'], function () {
+    // Admin Dashboard Routes
+    Route::get('/dashboard', 'Admin\DashboardController@dashboard')->name('admin.dashboard');
+  });
+});
+
+
+if (!app()->runningInConsole()) {
+  // Dynamic Routes
+  Route::group(['middleware' => ['setlang']], function () {
+
+    $wdPermalinks = Permalink::where('details', 0)->get();
+    foreach ($wdPermalinks as $pl) {
+      $type = $pl->type;
+      $permalink = $pl->permalink;
+
+      if ($type == 'admin_login') {
+        $action = 'Admin\LoginController@login';
+        $routeName = 'admin.login';
+        Route::get("$permalink", "$action")->name("$routeName")->middleware('guest:admin');
+        continue;
+      }
+    }
+  });
+}
